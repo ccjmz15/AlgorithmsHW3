d1f108e8a77c5bcd2c897c7ccc13054123d2e486
ccjmz15 <85185910+ccjmz15@users.noreply.github.com>
2022-03-03 19:40:01 -0500

    Lots of changes

--- algorithmsHW3.cpp
+++ algorithmsHW3.cpp
@@ -11,21 +11,23 @@ class Node {
 
 class Graph {
 	public:
-		Graph(vector<int> V, map<int, map<int, int>> E) {
-			vertices = V;
-			edges = E;
-			_numVertices = V.size();
+		Graph(int numVertices, map<int, map<int, int>> E) {
+			_numVertices = numVertices;
 			_numEdges = E.size();
 
 			initializeAdjacencyList(); // Creates an array of linked lists, each linked list is filled with NULL value
-			initializeVisited(); // Creates an array of size _numVertices and fills all values as 'false' or '0'
+			initializeAdjacencyMatrix();
+			resetVisited(); // Creates an array of size _numVertices and fills all values as 'false' or '0'
 
-			fillAdjacencyList(); // Create the initial adjacency list, uses Edges to compute
+			fillAdjacencyList(E); // Create the initial adjacency list, uses Edges to compute
+			fillAdjacencyMatrix();
 		}
 
-		void addEdge(int i, int j) { append(i, j); append(j, i); _numEdges++; } // Calls append twice, flipping the parameters.
+		~Graph() { deleteEntireAdjacencyList(); }
 
-		void deleteEdge(int i, int j);
+		void edgeAddition(int i, int j) { append(i, j); append(j, i); _numEdges++; } // Calls append twice, flipping the parameters.
+
+		void edgeDeletion(int i, int j);
 
 		void printAdjacencyList() {
 			cout << endl;
@@ -42,12 +44,77 @@ class Graph {
 			}
 		}
 
+		void printAdjacencyMatrix() {
+			for (int i = 0; i < _numVertices; i++) {
+				cout << endl;
+				for (int j = 0; j < _numVertices; j++) {
+					cout << '\t' << adjacencyMatrix[i][j];
+				}
+			}
+		}
+
 		void printVisited() { for (int i = 0; i < _numVertices; i++) { cout << visited[i] << endl; } }
 
-	protected:
-		void Components(); // computes the vertex sets of the connected components of G. Involves calling DFS(G, v), v = 0, 1, ..n – 1.
+		void DFS(int vertex) {
+			Node* head = adjacencyList[vertex];
+			vector<int> adjacentVertices = Visit(head);
+			int length = listLength(head);
 
-		void DFS(int vertex);
+			visited[vertex] = true;
+			printVisited();
+			cout << endl << endl;
+			for (int i = 0; i < length; i++) {
+				if (visited[adjacentVertices.at(i)] == false) {
+					DFS(adjacentVertices.at(i));
+				}
+			}
+		}
+
+	protected: // Basically all helper methods that cannot be called from outside the Graph class.
+
+		void Components() {// computes the vertex sets of the connected components of G. Involves calling DFS(G, v), v = 0, 1, ..n – 1.
+			
+			for (int i = 0; i < _numVertices; i++) { // Loop through each of the vertices
+				resetVisited(); // Reset the visited list to all false.
+				while (true) { // While DFS is running...
+					DFS(i);
+				
+				} 
+			}
+		
+		}
+
+		vector<int> Visit(Node* head) {
+			vector<int> vec;
+			while (head != NULL) {
+				vec.push_back(head->data);
+				head = head->next;
+			}
+			return vec;
+		}
+
+		int listLength(Node* head) {
+			if (head == NULL) { return 0; }
+			return 1 + listLength(head->next);
+		}
+
+		void initializeAdjacencyMatrix() {
+			bool** matrix = new bool*[_numVertices];
+			for (int i = 0; i < _numVertices; i++) { matrix[i] = new bool[_numVertices]; }
+			adjacencyMatrix = matrix;
+		}
+
+		void fillAdjacencyMatrix() {
+			for (int i = 0; i < _numVertices; i++) {
+				vector<int> connectedComponents = Visit(adjacencyList[i]);
+				for (int j = 0; j < _numVertices; j++) {
+					adjacencyMatrix[i][j] == false;
+					for (vector<int>::iterator itr = connectedComponents.begin(); itr != connectedComponents.end(); ++itr) {
+						adjacencyMatrix[i][*itr] == true;
+					}
+				}
+			}
+		}
 
 		void append(int i, int j) { // Appends vertex j to the end of linked list of vertex i
 			// 1. create and allocate node
@@ -71,18 +138,32 @@ class Graph {
 			return;
 		} 
 
-		void fillAdjacencyList() { 
+		void fillAdjacencyList(map<int, map<int, int>> originalEdges) {
 			map<int, map<int, int> >::iterator itr;
 			map<int, int>::iterator ptr;
 
-			for (itr = Edges.begin(); itr != Edges.end(); itr++) {
+			for (itr = originalEdges.begin(); itr != originalEdges.end(); itr++) {
 				for (ptr = itr->second.begin(); ptr != itr->second.end(); ptr++) {
-					addEdge(ptr->first, ptr->second);
+					edgeAddition(ptr->first, ptr->second);
 				}
 			}
 		}
 
-		void initializeVisited() { visited = new bool[_numVertices]; for (int i = 0; i < _numVertices; i++) { visited[i] = false; } }
+		void deleteEntireAdjacencyList() {
+			for (int i = 0; i < _numVertices; i++) {
+				Node* current = adjacencyList[i];
+				Node* next;
+				while (current != NULL) {
+					cout << current->data << "\t";
+					next = current->next;
+					free(current);
+					current = next;
+				}
+				adjacencyList[i] = NULL;
+			}	
+		}
+
+		void resetVisited() { visited = new bool[_numVertices]; for (int i = 0; i < _numVertices; i++) { visited[i] = false; } }
 
 		void initializeAdjacencyList() { adjacencyList = new Node * [_numVertices]; for (int i = 0; i < _numVertices; ++i) { adjacencyList[i] = NULL; } }
 
@@ -93,17 +174,16 @@ class Graph {
 		Node** adjacencyList;
 		Node* prev;
 
+		bool** adjacencyMatrix;
 		bool* visited;
-
-		vector<int> vertices;
-		map<int, map<int, int>> edges;
 };
 
 
-void printVertices(vector<int> listOfVertices) {
+
+void printVertices(int vertices) {
 	cout << "V = {";
-	for (int i = 0; i < listOfVertices.size() - 1; i++) { cout << listOfVertices.at(i) << ","; }
-	cout << listOfVertices.at(listOfVertices.size() - 1) << "}" << endl;
+	for (int i = 0; i < vertices - 1; i++) { cout << i << ","; }
+	cout << vertices - 1 << "}" << endl;
 }
 
 void printEdges(map<int, map<int, int>> edgeMapping) {
@@ -128,11 +208,6 @@ map<int, map<int,int>> addEdgeForE(map<int, map<int, int>> edgeMapping, int i, i
 	return edgeMapping;
 }
 
-vector<int> createVertices(int numVertices) {
-	vector<int> listOfVertices;
-	for (int counter = 0; counter < numVertices; counter++) { listOfVertices.push_back(counter); }
-	return listOfVertices;
-}
 
 int main() {
 	vector<int> V;
@@ -146,8 +221,6 @@ int main() {
 	cout << "Number of vertices = ";
 	cin >> numVertices;
 	cout << endl;
-
-	V = createVertices(numVertices);
 
 	while (true) {
 		if (loopCount % 2 == 0) {
@@ -167,12 +240,16 @@ int main() {
 	}
 	cout << endl << endl;
 
-	printVertices(V);
+	printVertices(numVertices);
 	printEdges(E);
 
 	cout << endl;
 
-	Graph graph = Graph(V, E);
+	Graph graph = Graph(numVertices, E);
 	graph.printVisited();
 	graph.printAdjacencyList();
+
+	graph.printAdjacencyMatrix();
+
+	graph.~Graph();
 }
